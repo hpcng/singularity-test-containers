df4b95a23763c4798fa25aeba019af169dd65449
David Trudgian <david.trudgian@sylabs.io>
2020-06-11 12:07:21 -0500

    Drop arm/v6 as some base images not present

--- build_push_docker.sh
+++ build_push_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PLATFORMS="linux/amd64,linux/arm64,linux/arm/v6,linux/arm/v7,linux/ppc64le"
+PLATFORMS="linux/amd64,linux/arm64,linux/arm/v7,linux/ppc64le"
 
 docker buildx build --platform "$PLATFORMS" -t sylabsio/aufs-sanity:latest --push docker-aufs-sanity
 docker buildx build --platform "$PLATFORMS" -t sylabsio/issue4525:latest --push docker-issue4525
